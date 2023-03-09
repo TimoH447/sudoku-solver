0889c04fc5e9adb86e8e116f51c201a2200dae5f
TimoH447 <timo.hauser66@gmail.com>
2023-03-09 02:16:15 +0100

    changed to tensorflow, first time testing prediction for sudoku, edited readme

--- README.md
+++ README.md
@@ -3,8 +3,28 @@ A web application for solving sudokus.
 
 See the result here:
 http://sudoku-solver.eu-central-1.elasticbeanstalk.com/ 
+## Solving Sudokus
 
 ## Sudoku Computer Vision
 
-![This is an image](doc/Sudoku_front.jpg)
-![This is an image](doc/Sudoku_front_transformed.jpg)
+The easiest way to solve a sudoku from a picture is when it is in the form of the following picture:
+![This is an image](doc/Sudoku_angle_transformed.jpg)
+In that case we can devide the width and height of the picture by 9 and then we can cut out each digit. 
+Thus we first need to be able to create from a normal picture
+![This is an image](doc/Sudoku_angle.jpg)
+a picture where we only see the sudoku and without an angle. For this we need to find 4 points from which we know where they lay in the picture with the changed perspective. These 4 point are the vertices of the sudoku. We know that they are connected by straight lines when we view them from the front.
+
+Therefore the first goal is to find the sudoku inside of a picture. We do this by tracing all contours in the picture and then choose the biggest, i.e. with the most pixels. This works very well due to the fact that when we take a picture from a sudoku it is usually the biggest object in the picture. Still this is a limitation since if there is an contour around the sudoku it won't work.
+But for our picture from above it works.
+![This is an image](doc/Sudoku_angle_contour.png)
+
+Now the next step is to identify the vertices in the contour of the sudoku. For this I go clockwise around the contour and write in a list the change in direction from one pixel to the next in the contour. I assign each direction a number: 0 - right, 1 - right/down, 2 - down, 3 - down/left, 4 - left, 5 - left/up, 6 - up, 7 - up/right.
+We can put these in a graph by counting through the pixels of the contour and get:
+![This is an image](doc/Sudoku_angle_directions.png)
+We can clearly by the points where the graph makes a step of two, that in these pixels are our vertices.
+
+Now when drawing circle around these pixels in the original image we indeed get the desired output.
+![This is an image](doc/Sudoku_angle_vertices.png)
+
+Putting this together we get the transformed image from start.
+
--- sudoku_app/sudoku/solver/sudoku_digit_extraction.py
+++ sudoku_app/sudoku/solver/sudoku_digit_extraction.py
@@ -1,171 +1,35 @@
 import json
-
-import torch
-from torch import nn 
-from torch.utils.data import DataLoader
-import torchvision.transforms as transforms
-from torchvision import datasets
-from torchvision.io import read_image
-from torchvision.transforms import ToTensor,ToPILImage
 from matplotlib import pyplot as plt
 from PIL import Image
+import numpy as np
+
+import tensorflow as tf
 
 import sudoku_cv
 
 ASSET_DIR = "/Users/timoh/OneDrive/Dokumente/Bildung/Programmieren/old_version/assets/"
 
-# Get cpu or gpu device for training.
-device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-print(f"Using {device} device")
+mnist = tf.keras.datasets.mnist
 
-training_data = datasets.MNIST(
-    root=ASSET_DIR+"data",
-    train=True,
-    download= True,
-    transform=ToTensor(),
-)
+(x_train,y_train),(x_test,y_test) = mnist.load_data()
+x_train,x_test = x_train/255.0,x_test/255.0
 
-test_data = datasets.MNIST(
-    root=ASSET_DIR+"data",
-    train=False,
-    download= True,
-    transform=ToTensor(),
-)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28,28)),
+    tf.keras.layers.Dense(128,activation="relu"),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+])
 
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
 
-def train(dataloader, model, loss_fn, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
-
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
-
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-
-def test(dataloader, model, loss_fn):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0, 0
-    with torch.no_grad():
-        for X, y in dataloader:
-            X, y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_fn(pred, y).item()
-            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-# Define model
-class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super(NeuralNetwork, self).__init__()
-        self.conv_layers = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2),
-            nn.Dropout2d(p=0.25)
-        )
-        self.linear_layers = nn.Sequential(
-            nn.Linear(9216, 128),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(128, 10),
-            nn.Softmax(dim=1)
-        )
-
-    def forward(self, x):
-        x = self.conv_layers(x)
-        x = x.view(x.size(0), -1)
-        x = self.linear_layers(x)
-        return x
-
-def make_model(training_data,test_data):
-    batch_size = 64
-
-   
-    #Create data loaders
-    train_dataloader = DataLoader(training_data,batch_size=batch_size)
-    test_dataloader = DataLoader(test_data,batch_size=batch_size)
-    
-    model = NeuralNetwork().to(device)
-    print(model)
-
-    loss_fn = nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
-
-    epochs = 5
-    for t in range(epochs):
-        print(f"Epoch {t+1}\n-------------------------------")
-        train(train_dataloader, model, loss_fn, optimizer)
-        test(test_dataloader, model, loss_fn)
-    print("Done!")
-
-    torch.save(model.state_dict(), ASSET_DIR+ "model.pth")
-    print("Saved PyTorch Model State to model.pth")
-
-def predict():
-    #make_model(training_data,test_data)
-
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load(ASSET_DIR+"model.pth"))
-
-    classes = ["0","1","2","3","4","5","6","7","8","9"]
-    #model.eval()
-    x, y = test_data[0][0], test_data[0][1]
-    print(test_data[0][0].shape)
-    with torch.no_grad():
-        pred = model(x)
-        predicted, actual = classes[pred[0].argmax(0)], classes[y]
-        print(f'Predicted: "{predicted}", Actual: "{actual}"')
-
-    plt.imshow(ToPILImage()(test_data[0][0]))
-    plt.show()
-
-def predict_digit(image):
-    model = NeuralNetwork()
-    model.load_state_dict(torch.load(ASSET_DIR+"mnist-b07bb66b.pth", map_location=torch.device('cpu')))
-    
-
-    classes = ["0","1","2","3","4","5","6","7","8","9"]
-    #model.eval()
-    
-    transform = transforms.Compose([
-        transforms.Resize((28,28)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485],
-                         std=[0.229])
-                         ])
-    input_tensor = transform(image)
-
-    # Apply the transformation pipeline to the image
-
-    input_tensor = input_tensor.view(1,1,28,28)
-    
-    with torch.no_grad():
-        output = model(input_tensor)
-
-    # Convert the output to a human-readable format
-    print(output)
-    prediction = classes[output[0].argmax(0)]
-    print(prediction)
 
 def digit_images_from_sudoku(image_path):
-    sudoku = sudoku_cv.extract_sudoku(image_path=ASSET_DIR+"Sudoku_front.jpg",saving=False)
+    sudoku = sudoku_cv.extract_sudoku(image_path=image_path,saving=False)
 
     digits = []
     for i in range(9):
@@ -214,8 +78,28 @@ def digit_preprocess(digit_img):
     digit_img = digit_img.crop((1,1,29,29))
     digit_img = sudoku_cv.grayscale_img(digit_img)
     digit_img = sudoku_cv.adaptive_thresholding(digit_img)
-    return digit_img
+    np_image= sudoku_cv.pillow2np(digit_img)
+    np_image = np_image / 255.0
+    np_image=np_image.astype('float32')
+    np_image = np.reshape(np_image,(1,28,28))
+    
+    return np_image
+
+def get_prediction(images):
+    model.fit(x_train, y_train, epochs=5)
+
+    probability_model = tf.keras.Sequential([
+        model,
+        tf.keras.layers.Softmax()
+    ])
+    for index,image in enumerate(images):
+        print(index)
+        print(probability_model.predict(image))
 
 if __name__=="__main__":
-    #sudoku_cv.extract_sudoku(ASSET_DIR + "Sudoku_front.jpg")
-    pass    
+    digits = digit_images_from_sudoku(ASSET_DIR+"Sudoku_front.jpg") 
+    digits = [digit_preprocess(digit) for digit in digits]
+    #digits = np.expand_dims(digits,-1)
+    get_prediction(digits)
+
+    
